51c611630e33c69c1bf22f4a64e81cdc29e2a277
kuyasawa <k.sawa.tttr@gmail.com>
2022-10-11 19:55:19 +0900

    サーバーアップ用

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
 var bodyParser = require('body-parser');
 
 
 var indexRouter = require('./routes/index');
-var editRouter = require('./routes/edit');
 
 var app = express();
 app.use(helmet());
+app.listen(32768);
 
 //モデルの読み込み
-var News = require ('./models/news-model');
-News.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,12 +21,11 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
-app.use('/edit', editRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,8 +39,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  //res.status(err.status || 500);
+  //res.render('error');
 });
 
 
