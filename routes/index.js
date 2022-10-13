a21a30c97deacd5e70ed293ee4bed84727c53a61
kuyasawa <k.sawa.tttr@gmail.com>
2022-10-13 17:32:55 +0900

    微修正

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Kuya Sawa Official Page' });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
   res.render('about');
 });
 
-router.get('/works', function(req, res, next) {
+router.get('/works', function(req, res) {
   res.render('works');
 });
 
-router.get('/link', function(req, res, next) {
+router.get('/link', function(req, res) {
   res.render('link');
 });
 
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function(req, res) {
   res.render('contact');
 });
 
-router.get('/works/commercial', function(req, res, next) {
+router.get('/works/commercial', function(req, res) {
   res.render('commercial');
 });
-router.get('/works/portrait', function(req, res, next) {
+router.get('/works/portrait', function(req, res) {
   res.render('portrait');
 });
-router.get('/works/runway', function(req, res, next) {
+router.get('/works/runway', function(req, res) {
   res.render('runway');
 });
-router.get('/works/movie', function(req, res, next) {
+router.get('/works/movie', function(req, res) {
   res.render('movie');
 });
 
